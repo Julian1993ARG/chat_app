2abf690a96eb065f97953e35e45217a3d3c4c808
julian <julianmartinez_93@hotmail.com>
2024-04-02 16:05:15 -0300

    list view

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
-  final String imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-B4E38DsRjoS9V7600ZwbzCNK4NxMVxNhfA&s';
+  final String imageUrl =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-B4E38DsRjoS9V7600ZwbzCNK4NxMVxNhfA&s';
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat'),
@@ -17,7 +17,30 @@ class ChatScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Text('Message $index');
+              },
+            )),
+            Text("Type a message")
+          ],
+        ),
+      ),
     );
   }
 }
