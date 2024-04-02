a9dc79b85cdfbed0a1d22df0ebe86342ce905427
julian <julianmartinez_93@hotmail.com>
2024-04-02 15:41:33 -0300

    app bar with profile image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/config/theme/app_theme.dart';
+import 'package:chat_app/presentation/chat/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -12,17 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Chat app',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 1).theme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Chat app'),
-        ),
-        body: Center(
-          child: FilledButton.tonal(
-            onPressed: (){}, 
-            child: const Text('Click me')
-            )
-        ),
-      ),
+      home: const ChatScreen(),
     );
   }
 }
---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+  final String imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-B4E38DsRjoS9V7600ZwbzCNK4NxMVxNhfA&s';
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat'),
+        centerTitle: true,
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(imageUrl),
+          ),
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}
